a508acfce7ffcc0fbfe3fdfcc76f8db7c72e72a7
g-pooja-03 <98845817+g-pooja-03@users.noreply.github.com>
2022-12-13 16:07:10 -0500

    Updated with our app file

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,17 +1,65 @@
-###
-# Main application interface
-###
+# import the Flask framework
+from flask import Flask, jsonify
+from flaskext.mysql import MySQL
 
-# import the create app function 
-# that lives in src/__init__.py
-from src import create_app
+# create a flask object
+app = Flask(__name__)
 
-# create the app object
-app = create_app()
+# add db config variables to the app object
+app.config['MYSQL_DATABASE_HOST'] = 'db'
+app.config['MYSQL_DATABASE_PORT'] = 3306
+app.config['MYSQL_DATABASE_USER'] = 'webapp'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'abc123'
+app.config['MYSQL_DATABASE_DB'] = 'housing_finder'
 
-if __name__ == '__main__':
-    # we want to run in debug mode (for hot reloading) 
-    # this app will be bound to port 4000. 
-    # Take a look at the docker-compose.yml to see 
-    # what port this might be mapped to... 
+# create the MySQL object and connect it to the 
+# Flask app object
+db_connection = MySQL()
+db_connection.init_app(app)
+
+# import the blueprint objects from their respective locations
+from customer_api.customers import customers_blueprint
+from manager_api.managers import managers_blueprint
+
+# register the blueprints we created with the current Flask app object.
+app.register_blueprint(customers_blueprint, url_prefix='/cust')
+app.register_blueprint(managers_blueprint, url_prefix='/mgr')
+
+# --------------------------------------------------------------------
+# Create a function named hello world that 
+# returns a simple html string 
+# the @app.route("/") connects the hello_world function to 
+# the URL / 
+@app.route("/")
+def hello_world():
+    return f'<h1>Hello From the Flask-MySQL Connection Tutorial</h1>'
+
+@app.route('/pref')
+def pref():
+   cur = db_connection.get_db().cursor()
+   cur.execute('select * from Roommate_Pref')
+   row_headers = [x[0] for x in cur.description]
+   json_data = []
+   theData = cur.fetchall()
+   for row in theData:
+       json_data.append(dict(zip(row_headers, row)))
+   return jsonify(json_data)
+
+@app.route('/price')
+def price():
+   cur = db_connection.get_db().cursor()
+   cur.execute('select * from Price_Code')
+   row_headers = [x[0] for x in cur.description]
+   json_data = []
+   theData = cur.fetchall()
+   for row in theData:
+       json_data.append(dict(zip(row_headers, row)))
+   return jsonify(json_data)
+
+# If this file is being run directly, then run the application 
+# via the app object. 
+# debug = True will provide helpful debugging information and 
+#   allow hot reloading of the source code as you make edits and 
+#   save the files. 
+if __name__ == '__main__': 
     app.run(debug = True, host = '0.0.0.0', port = 4000)
